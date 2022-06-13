518283b1e3085a8db1993ec467f1ff6e6acc2bb5
Mathew Dony <mathewdony007@gmail.com>
2022-06-13 22:30:02 +1000

    Add features component

--- airbnb-clone/src/components/index.ts
+++ airbnb-clone/src/components/index.ts
@@ -4,4 +4,6 @@ export { default as Hero } from './Hero';
 
 export { default as TryHosting } from './TryHosting';
 
+export { default as Features } from './Features';
+
 export { default as Footer } from './Footer';
