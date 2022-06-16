d7ddde179805295525433d2529811653e87f6cde
Mathew Dony <mathewdony007@gmail.com>
2022-06-16 22:18:46 +1000

    Review section done

--- airbnb-clone/src/components/Reviews.tsx
+++ airbnb-clone/src/components/Reviews.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { grey } from "../constants/ui/colors";
+import { black, grey, darkGrey } from "../constants/ui/colors";
 import { device } from "../constants/ui/breakpoints";
 
 type Props = {};
@@ -17,25 +17,34 @@ const Reviews = (props: Props) => {
     {
       picture: "home1",
       star: 5,
-      description: "It was great",
+      description:
+        "“I absolutely loved our stay at this property. Located in a quiet street a short walk to restaurants and cafes in North Hobart, it was even better than the pictures”",
       name: "Downey",
       location: "Australia",
     },
     {
       picture: "home2",
-      star: 4,
-      description: "It was cool",
+      star: 5,
+      description:
+        "“A fantastic apartment with a wonderful host!  The apartment was warm and cosy (despite the 2-degree cold outside), with plenty of space for two, and beautifully decorated throughout.”",
       name: "Evans",
       location: "Australia",
     },
     {
       picture: "home3",
-      star: 5,
-      description: "It was alright",
+      star: 4,
+      description:
+        "“We really appreciated you making it so easy for our late arrival. We really enjoyed the balcony and the cafe next door - delicious. Thank you also for the wine - we will be back!”",
       name: "Hemsworth",
       location: "Australia",
     },
   ];
+
+  const renderStars = (stars: number) => {
+    return [...Array(stars)].map((e, i) => (
+      <Star src={`/src/images/svg/color-star.svg`} />
+    ));
+  };
 
   return (
     <StyledReviews>
@@ -48,13 +57,23 @@ const Reviews = (props: Props) => {
       </TextContainer>
       <ReviewsContainer>
         {reviewsData.map((item, index) => (
-          <div>
+          <div className="review-wrapper">
             <img
               className="house-image"
               src={`/src/images/reviews/${item.picture}.webp`}
             />
-            <p>{item.name}</p>
-            <p>{item.description}</p>
+            <div className="star-wrapper">{renderStars(item.star)}</div>
+            <p className="description">{item.description}</p>
+            <div className="host-wrapper">
+              <img
+                src={`/src/images/reviews/${item.name}.jpeg`}
+                className="customer-image"
+              />
+              <div>
+                <p className="name">{item.name}</p>
+                <p className="location">{item.location}</p>
+              </div>
+            </div>
           </div>
         ))}
       </ReviewsContainer>
@@ -79,8 +98,22 @@ export const StyledReviews = styled.div`
   }
 `;
 
+export const Star = styled.img`
+  margin-left: 4px;
+  height: 16px;
+  width: 16px;
+`;
+
 export const TextContainer = styled.div`
   margin-bottom: 20px;
+  grid-column: span 12;
+
+  @media ${device.TABLET} {
+    h2 {
+      font-size: 26px;
+      line-height: 30px;
+    }
+  }
 `;
 
 export const ReviewsContainer = styled.div`
@@ -88,14 +121,62 @@ export const ReviewsContainer = styled.div`
   grid-template-columns: repeat(12, 1fr);
   grid-column-gap: 16px;
   grid-row-gap: 24px;
-  grid-column: span 12;
+
+  .star-wrapper {
+    margin: 12px 0;
+  }
+
+  .review-wrapper {
+    grid-column: span 12;
+  }
 
   .house-image {
+    width: 100%;
     border-radius: 12px;
   }
 
+  .description {
+    color: ${darkGrey};
+    line-height: 20px;
+    margin-top: 6px;
+    margin-bottom: 12px;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+
+  .name {
+    color: ${black};
+    line-height: 20px;
+    font-size: 16px;
+    font-weight: 800;
+    padding-left: 12px;
+    margin-top: 0;
+  }
+
+  .location {
+    font-size: 14px;
+    line-height: 18px;
+    padding-left: 12px;
+    margin-top: 0;
+  }
+
+  .host-wrapper {
+    display: flex;
+    align-items: center;
+  }
+
+  .customer-image {
+    height: 48px;
+    width: 48px;
+    border-radius: 50%;
+  }
+
   @media ${device.TABLET} {
-    grid-column: span 4;
+    .review-wrapper {
+      grid-column: span 4;
+    }
   }
 `;
 
--- airbnb-clone/src/constants/ui/colors.ts
+++ airbnb-clone/src/constants/ui/colors.ts
@@ -2,6 +2,8 @@ export const black = '#222222';
 
 export const grey = '#717171';
 
+export const darkGrey = '#484848';
+
 export const lightGrey = '#EBEBEB';
 
 export const primary = '#e51c57s';
