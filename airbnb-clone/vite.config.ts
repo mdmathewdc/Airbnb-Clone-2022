445259c69f5c3b523f3f20750490972299e1e6e5
Mathew Dony <mathewdony007@gmail.com>
2022-06-17 10:08:30 +1000

    Fix image build command

--- airbnb-clone/vite.config.ts
+++ airbnb-clone/vite.config.ts
@@ -4,5 +4,8 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()]
+  plugins: [react(), svgr()],
+  build: {
+    assetsInlineLimit: 2048 // Otherwise images upto this size won't be part of the build
+  }
 })
