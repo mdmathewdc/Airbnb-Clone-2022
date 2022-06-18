32d29ce7fe25b2b6677cd0add705adb7f06b72a1
Mathew Dony <mathewdony007@gmail.com>
2022-06-19 09:51:30 +1000

    Finished input work

--- airbnb-clone/src/components/ui/input/Location.tsx
+++ airbnb-clone/src/components/ui/input/Location.tsx
@@ -9,19 +9,23 @@ const Location = (props: Props) => {
   return (
     <StyledLocation>
       <BoldText text="LOCATION" margin={"0"} />
-      <input type="text" />
+      <input type="text" placeholder="Anywhere" />
     </StyledLocation>
   );
 };
 
 export const StyledLocation = styled.div`
-  padding: 8px 12px;
+  padding: 12px 16px 10px 16px;
   border: 1px solid ${lightGrey};
   border-radius: 8px;
 
   input {
     border: none;
+    padding: 4px 0 0 0;
 
+    &::placeholder {
+      font-size: 16px;
+    }
     &:focus {
       outline: none;
     }
