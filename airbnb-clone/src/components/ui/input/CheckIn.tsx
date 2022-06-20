a82f3c7b2955236f992514284be6da0066710606
Mathew Dony <mathewdony007@gmail.com>
2022-06-20 19:35:38 +1000

    Dates done

--- airbnb-clone/src/components/ui/input/CheckIn.tsx
+++ airbnb-clone/src/components/ui/input/CheckIn.tsx
@@ -7,20 +7,60 @@ import useInputFocus from "../../hooks/useInputFocus";
 type Props = {};
 
 const CheckIn = (props: Props) => {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const checkinRef = useRef<HTMLInputElement>(null);
+  const checkoutRef = useRef<HTMLInputElement>(null);
 
-  const inputFocused = useInputFocus(inputRef);
+  const checkinFocus = useInputFocus(checkinRef);
+  const checkoutFocus = useInputFocus(checkoutRef);
 
   return (
-    <StyledCheckIn focused={inputFocused}>
-      <BoldText text="CHECK IN" margin={"0"} />
-      <input ref={inputRef} type="text" placeholder="Anywhere" />
-    </StyledCheckIn>
+    <DateWrapper checkinFocus={checkinFocus} checkoutFocus={checkoutFocus}>
+      <StyledCheck className="check-in" focused={checkinFocus}>
+        <BoldText text="CHECK IN" margin={"0"} />
+        <input ref={checkinRef} type="text" placeholder="Add Date" />
+      </StyledCheck>
+      <span> </span>
+      <StyledCheck className="check-out" focused={checkoutFocus}>
+        <BoldText text="CHECK OUT" margin={"0"} />
+        <input ref={checkoutRef} type="text" placeholder="Add Date" />
+      </StyledCheck>
+    </DateWrapper>
   );
 };
 
-export const StyledCheckIn = styled.div<{ focused: boolean }>`
+const DateWrapper = styled.div<{
+  checkinFocus: boolean;
+  checkoutFocus: boolean;
+}>`
+  display: flex;
+  margin-top: 12px;
+
+  .check-in {
+    border-right: none;
+    border-top-right-radius: ${(props) => (props.checkinFocus ? "8px" : "0")};
+    border-bottom-right-radius: ${(props) =>
+      props.checkinFocus ? "8px" : "0"};
+  }
+
+  .check-out {
+    border-left: none;
+    border-top-left-radius: ${(props) => (props.checkoutFocus ? "8px" : "0")};
+    border-bottom-left-radius: ${(props) =>
+      props.checkoutFocus ? "8px" : "0"};
+  }
+
+  span {
+    margin: 8px 0;
+    width: 1px;
+    background: ${(props) =>
+      props.checkinFocus || props.checkoutFocus ? "none" : lightGrey};
+    position: relative;
+  }
+`;
+
+export const StyledCheck = styled.div<{ focused: boolean }>`
   position: relative;
+  width: 50%;
   padding: 10px 16px;
   border: 1px solid ${lightGrey};
   border-radius: 8px;
